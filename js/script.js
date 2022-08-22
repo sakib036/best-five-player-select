a054f61097e8db3187013f2bc850c17ac9395385
Md. Nazmus Sakib <sakib036@gmail.com>
2022-08-22 18:28:57 +0600

    header-background-selet

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-// Top right side navber in main section
+// Top right side navber in main section 
+// selected five
 let cnt=0;
 function selectPlayer(player){
     // count number
@@ -15,11 +16,12 @@ function selectPlayer(player){
         player.parentNode.children[2].disabled=true;
         player.parentNode.children[2].style.backgroundColor="gray";
     }
+    // if more than five then worning
     else{
         alert('You Can Not Select More than five')
     } 
 }
-// get input value in input  field
+// get input value in input  field common Function
 function getTheValueOfInput(inptId){
     const getInputValueById=document.getElementById(inptId);
         const getInputValueByIdString=getInputValueById.value;
@@ -27,7 +29,7 @@ function getTheValueOfInput(inptId){
            return (getInputValue); 
           
 }
-// set the value in target place
+// set the value in target place common Function
 function innerTextByID(innerTextId, value){
     const innerTextValueByID=document.getElementById(innerTextId);
     const innerTextValueByIDString=innerTextValueByID.innerText;
@@ -35,19 +37,24 @@ function innerTextByID(innerTextId, value){
     innerTextValueByID.innerText=value;
     return (value);
 }
+// when calculator button click 
 document.getElementById('btn-calculate').addEventListener('click',function(){
     const getPlayerAmount=getTheValueOfInput('player-amount');
+    // if per playe amount input is a number and geter than 0 then
     if(getPlayerAmount>=0){
         const playerSelect=document.getElementById('selecct-player')
         const nomberOfPlayer=playerSelect.children.length;
+        // if select any player
         if(nomberOfPlayer>0){
             const totalPlayerAmount=getPlayerAmount*nomberOfPlayer;
             const totalPlayerExpenceValue=innerTextByID('player-expence',totalPlayerAmount);
         }
+        // Not select any player
         else{
             alert('Please Select a Player') ;
         }
     } 
+    // if per playe amount input is a string and less than 0 then
     else{
         alert('Please Enter a correct Number');
     }
@@ -56,13 +63,18 @@ document.getElementById('btn-calculate').addEventListener('click',function(){
 
 })
 document.getElementById('btn-total').addEventListener('click',function(){
+    // manager input value
     const managerExpence=getTheValueOfInput('manager-expence');
+    // coach input value
     const coachExpence=getTheValueOfInput('coach-expence');
+    // Manager and coach input value check
     if(managerExpence>=0 && coachExpence>=0){
+        // get the value of player Expence 
         const valeOfPlayerExpence=document.getElementById('player-expence');
         const valeOfPlayerExpenceString=valeOfPlayerExpence.innerText;
         const totalPlayerExpence=parseInt(valeOfPlayerExpenceString);
         if(totalPlayerExpence>0){
+          // Total sum and assign value total 
             const totalExpence=managerExpence+coachExpence+totalPlayerExpence;
             const totalExpenceValue=innerTextByID('total-expence',totalExpence);
         }
