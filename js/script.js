82c92c6b43c7df0193932f966470955e1d4f6d22
Md. Nazmus Sakib <sakib036@gmail.com>
2022-08-22 13:56:46 +0600

    card-picture-change

--- js/script.js
+++ js/script.js
@@ -17,6 +17,60 @@ function selectPlayer(player){
     }
     else{
         alert('You Can Not Select More than five')
-    }
-   
+    } 
 }
+// get input value in input  field
+function getTheValueOfInput(inptId){
+    const getInputValueById=document.getElementById(inptId);
+        const getInputValueByIdString=getInputValueById.value;
+            const getInputValue=parseInt(getInputValueByIdString);
+           return (getInputValue); 
+          
+}
+// set the value in target place
+function innerTextByID(innerTextId, value){
+    const innerTextValueByID=document.getElementById(innerTextId);
+    const innerTextValueByIDString=innerTextValueByID.innerText;
+    const innerTexValue=parseInt(innerTextValueByIDString);
+    innerTextValueByID.innerText=value;
+    return (value);
+}
+document.getElementById('btn-calculate').addEventListener('click',function(){
+    const getPlayerAmount=getTheValueOfInput('player-amount');
+    if(getPlayerAmount>=0){
+        const playerSelect=document.getElementById('selecct-player')
+        const nomberOfPlayer=playerSelect.children.length;
+        if(nomberOfPlayer>0){
+            const totalPlayerAmount=getPlayerAmount*nomberOfPlayer;
+            const totalPlayerExpenceValue=innerTextByID('player-expence',totalPlayerAmount);
+        }
+        else{
+            alert('Please Select a Player') ;
+        }
+    } 
+    else{
+        alert('Please Enter a correct Number');
+    }
+    
+
+
+})
+document.getElementById('btn-total').addEventListener('click',function(){
+    const managerExpence=getTheValueOfInput('manager-expence');
+    const coachExpence=getTheValueOfInput('coach-expence');
+    if(managerExpence>=0 && coachExpence>=0){
+        const valeOfPlayerExpence=document.getElementById('player-expence');
+        const valeOfPlayerExpenceString=valeOfPlayerExpence.innerText;
+        const totalPlayerExpence=parseInt(valeOfPlayerExpenceString);
+        if(totalPlayerExpence>0){
+            const totalExpence=managerExpence+coachExpence+totalPlayerExpence;
+            const totalExpenceValue=innerTextByID('total-expence',totalExpence);
+        }
+        else{
+            alert('Per-Player Amount Or Player Selection Empty')
+        }
+    }
+    else{
+        alert('Please Enter a correct Number');
+    }
+})
